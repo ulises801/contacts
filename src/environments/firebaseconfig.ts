6416549101469823ceb9972f2087d9ecb6159433
ulises801 <ulises-801@outlook.es>
2022-05-29 22:03:33 -0500

    v2.0

---
+++ src/environments/firebaseconfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAWVPO52VXYvGYwIjt7kuzSZSpgYL85i38",
+  authDomain: "contact-app-3f57d.firebaseapp.com",
+  projectId: "contact-app-3f57d",
+  storageBucket: "contact-app-3f57d.appspot.com",
+  messagingSenderId: "499552342120",
+  appId: "1:499552342120:web:00bc697853a5b9b2be2bdd",
+  measurementId: "G-6FWHF5FDJ7"
+};
